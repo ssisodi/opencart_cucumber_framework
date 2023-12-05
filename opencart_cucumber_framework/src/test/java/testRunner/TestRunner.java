03d642ca0c3d4b47016c12fe90dd25bcc842b148
Siddhanth Sisodia <sisodiasiddhanth@gmail.com>
2023-12-05 14:19:30 +0530

    POC_Update2

--- opencart_cucumber_framework/src/test/java/testRunner/TestRunner.java
+++ opencart_cucumber_framework/src/test/java/testRunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+@RunWith(Cucumber.class) 
 @CucumberOptions(
 
         features= {".//Features//Login.feature"},
